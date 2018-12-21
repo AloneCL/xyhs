cfdb4f73b5666025238c490eafd53804b818368f
ALONGER_C <along9271_c@qq.com>
2018-12-21 17:08:28 +0800

    提交测试

---
+++ xyhs/src/cgl/xyhs/pojo/User.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package cgl.xyhs.pojo;
+
+/**
+* @ClassName: User.java
+* @Description: 用户类
+* @version: v1.0.0
+* @author: 陈光磊
+* @date: 2018年12月21日 下午5:07:31 
+ */
+public class User {
+     private String name;
+}
